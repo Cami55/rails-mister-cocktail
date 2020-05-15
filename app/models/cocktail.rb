e5eeacf7794612f537274de4a50965ad04d40026
camelia svennes <camelia.svennes@gmail.com>
2020-05-15 16:48:58 +0100

    cl

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,5 @@
 class Cocktail < ApplicationRecord
-  has_many :doses,  dependent: :destroy
+  has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, presence: true, allow_blank: false, uniqueness: true
   has_one_attached :photo
